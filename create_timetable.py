7a1dfcfb0d18b3bd87f8fb6c1306058509576dd2
sanglee <pksll325@gmail.com>
2019-02-04 18:55:27 +0900

    #10 timetable for only O's

--- create_timetable.py
+++ create_timetable.py
@@ -1,53 +1,34 @@
 import read_csv as rcsv
-#import employee as E
 import timetable as tt
 from openpyxl import load_workbook
 
 schedule = tt.timetable()
+schedule.set_cell()
 schedule.set_max()
 
-for k in range(len(rcsv.student)):
-    for i in range(9):
-        for j in range(5):
-            if schedule.cell[i][j].current < schedule.cell[i][j].max:
-                if rcsv.student[k].week[i][j] == 'O':
-                    schedule.cell[i][j].name.append(rcsv.student[k].name)
-                    schedule.cell[i][j].current += 1
+#load O info from the data
+for i in range(9):
+    for j in range(5):
+        for k in range(len(rcsv.student)):
+            if rcsv.student[k].week[i][j] == 'O':
+                schedule.cell[i][j].name.append(rcsv.student[k].name)
+                schedule.cell[i][j].current = schedule.cell[i][j].current+1
 
 
 wb = load_workbook('result.xlsx')
 ws1 = wb.active
 ws1.title = "근무시간표"
 
-for i in range(2):
+line = 0 #number of lines accumulated
+for i in range(9):
     for j in range(5):
-        if i < schedule.cell[i][j].current:
-            ws1.cell(row=3+i, column=3+j).value = schedule.cell[0][j].name[i]
-        else:
-            ws1.cell(row=3+i, column=3+j).value = " "
-
-for i in range(3):
-    for j in range(5):
-        if i < schedule.cell[i][j].current:
-            ws1.cell(row=5+i, column=3+j).value = schedule.cell[1][j].name[i]
-        else:
-            ws1.cell(row=5+i, column=3+j).value = " "
-
-for k in range(4):
-    for i in range(4):
-        for j in range(5):
-            if i < schedule.cell[i][j].current:
-                ws1.cell(row=(8+4*k)+i, column=3+j).value = schedule.cell[2+k][j].name[i] 
+        if i > 0 and j == 0:
+            line += schedule.cell[i-1][j].max
+        for k in range(schedule.cell[i][j].max):
+            if k < len(schedule.cell[i][j].name):
+                ws1.cell(row=3+line+k, column=3+j).value = schedule.cell[i][j].name[k]
             else:
-                ws1.cell(row=(8+4*k)+i, column=3+j).value = " "
-
-for k in range(3):
-    for i in range(3):
-        for j in range(5):
-            if i < len(schedule.cell[i][j].name):
-                ws1.cell(row=(24+k*3)+i, column=3+j).value = schedule.cell[6+k][j].name[i]
-            else:
-                ws1.cell(row=(24+k*3)+i, column=3+j).value = " "
+                ws1.cell(row=3+line+k, column=3+j).value = " "
 
 
 wb.save('result.xlsx')
